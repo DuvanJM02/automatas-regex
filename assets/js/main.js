45a380c5251d5b5646cbc0a26961fef5473db4d4
Webmaster Bavpublicidad <duvanlol02@gmail.com>
2022-09-26 23:33:38 -0500

    avc

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,16 @@
+// document.getElementById('name').addEventListener('input', (e)=>{
+//     document.getElementById('nombre').innerHTML = e.target.value;    
+// });
+
+const REGEXNAME = /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+)(\s+([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,1}[A-Za-zÑñÁáÉéÍíÓóÚú]+))*$/;
+
+let names = document.getElementById('name').value;
+
 document.getElementById('name').addEventListener('input', (e)=>{
-    document.getElementById('nombre').innerHTML = e.target.value;    
+    if(REGEXNAME.test(names)){
+        document.getElementById('nombre').innerHTML = e.target.value;   
+    }else{
+        document.getElementById('nombre').innerHTML = "Regex incorrecta";   
+    }
 });
+
