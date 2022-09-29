01d2f53ddad493501b241d9c190fe4ffd269059e
djaramillom1 <github@bavpublicidad.com>
2022-09-29 14:20:25 -0500

    compro
    comprobacion
    
    Co-Authored-By: Duván Jaramillo <60822264+DuvanJM02@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,70 +6,105 @@ const REGEXNAME = /^([A-Za-zÑñÁáÉéÍíÓóÚú]+['\-]{0,50}[A-Za-zÑñÁá
 const REGEXCODE = /^(?!0|1$)\d{1,8}$/;
 const REGEXDATE = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
 const REGEXLOC = /[A-Za-z0-9#'\.\-\s\,]$/;
-const REGEXTEL = /[6][0-9]{0,6}$/;
-const REGEXCEL = /[3][0-9]{0,9}$/;
+const REGEXTEL = /^(6)[0-9]{0,6}$/;
+const REGEXCEL = /^(3)[0-9]{0,9}$/;
 const REGEXEMAIL = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
 let n = document.getElementById('name').addEventListener('input', (e)=>{
     let names = document.getElementById('name').value;
-    if(REGEXNAME.test(names)){
-        document.getElementById('name-error').innerHTML = ''; 
-        document.getElementById('nombre').innerHTML = e.target.value;   
-    }else{
-        document.getElementById('name-error').innerHTML = "Ingrese un nombre válido";   
-    }
+    validateForm(e, REGEXNAME, names);
 });
 
 let c = document.getElementById('code').addEventListener('input', (e)=>{
     let code = document.getElementById('code').value;
-    if(REGEXCODE.test(code)){
-        document.getElementById('code-error').innerHTML = ''; 
-    }else{
-        document.getElementById('code-error').innerHTML = "Ingrese un código válido (solo números)";   
-    }
+    validateForm(e, REGEXCODE, code);
 });
 
 let d = document.getElementById('date').addEventListener('input', (e)=>{
     let date = document.getElementById('date').value;
-    if(REGEXDATE.test(date)){
-        document.getElementById('date-error').innerHTML = ''; 
-    }else{
-        document.getElementById('date-error').innerHTML = "Ingrese una fecha válida con este formato (dd-mm-YYYY)";   
-    }
+    validateForm(e, REGEXDATE, date);
 });
 
 let l = document.getElementById('location').addEventListener('input', (e)=>{
     let location = document.getElementById('location').value;
-    if(REGEXLOC.test(location)){
-        document.getElementById('loc-error').innerHTML = ''; 
-    }else{
-        document.getElementById('loc-error').innerHTML = "Ingrese una ubicación válida";   
-    }
+    validateForm(e, REGEXLOC, location);
 });
 
 let t = document.getElementById('phone').addEventListener('input', (e)=>{
     let tel = document.getElementById('phone').value;
-    if(REGEXTEL.test(tel)){
-        document.getElementById('tel-error').innerHTML = ''; 
-    }else{
-        document.getElementById('tel-error').innerHTML = "Ingrese número válido y menor a 7 dígitos.";   
-    }
+    validateForm(e, REGEXTEL, tel);
 });
 
 let cel = document.getElementById('cel').addEventListener('input', (e)=>{
     let tel_cel = document.getElementById('cel').value;
-    if(REGEXCEL.test(tel_cel)){
-        document.getElementById('cel-error').innerHTML = ''; 
-    }else{
-        document.gecelementById('cel-error').innerHTML = "Ingrese número válido y menor a 10 dígitos.";   
-    }
+    validateForm(e, REGEXCEL, tel_cel);
 });
 
-// let c = document.getElementById('phone').addEventListener('input', (e)=>{
-//     let tel = document.getElementById('phone').value;
-//     if(REGEXTEL.test(tel)){
-//         document.getElementById('tel-error').innerHTML = ''; 
-//     }else{
-//         document.getElementById('tel-error').innerHTML = "Ingrese número válido y menor a 10 dígitos.";   
-//     }
-// });
+let em = document.getElementById('email').addEventListener('input', (e)=>{
+    let email = document.getElementById('email').value;
+    validateForm(e, REGEXEMAIL, email);
+});
+
+function validateForm(event, regn, dato){
+    console.log(event.target.id)
+    if(arrayInputs() == false){
+        document.getElementById('enviar').setAttribute('disabled', 'true');
+    }else{
+        document.getElementById('enviar').removeAttribute('disabled');
+    }
+    if(regn.test(dato)){
+        document.getElementById(event.target.id + '-error').innerHTML = ''; 
+        document.getElementById('enviar').removeAttribute('disabled');
+    }else{
+        document.getElementById('enviar').setAttribute('disabled', 'true');
+        switch (event.target.id) {
+            case "name":
+                document.getElementById(event.target.id + '-error').innerHTML = "Ingrese un nombre válido.";  
+                break;
+            case "code":
+                document.getElementById(event.target.id + '-error').innerHTML = "Ingrese un código válido (solo números).";  
+                break;
+            case "date":
+                document.getElementById(event.target.id + '-error').innerHTML = "Ingrese una fecha válida con este formato (dd-mm-YYYY)";  
+                break;
+            case "location":
+                document.getElementById(event.target.id + '-error').innerHTML = "Ingrese una ubicación válida.";  
+                break;
+            case "phone":
+                document.getElementById(event.target.id + '-error').innerHTML = "Ingrese número válido que empiece por 6 y menor a 7 dígitos.";  
+                break;
+            case "cel":
+                document.getElementById(event.target.id + '-error').innerHTML = "Ingrese número válido que debe empezar por el numero 3 y menor a 10 dígitos.";  
+                break;
+            case "email":
+                document.getElementById(event.target.id + '-error').innerHTML = "Ingrese un correo válido.";  
+                break;
+            default:
+                console.log('rellene bien los campos')
+                break;
+        } 
+    }
+}
+
+function arrayInputs(){
+    let inputs = document.querySelectorAll('input');
+
+    for(let i; i <= inputs.length; i++){
+        console.log(inputs);
+        // if(inputs.querySelectorAll('input')[i].value == ''){
+        //     return false;
+        // }else{
+        //     return true;
+        // }
+    }
+}
+
+console.log(arrayInputs());
+
+function saveStudent(e){
+    e.preventDefault();
+
+    arrayInputs();
+
+    return;
+}
